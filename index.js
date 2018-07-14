474cc61c1f2d5125cdc66dee06435a14e4190e43
Jarrad Law <jarrad.law@gmail.com>
2018-07-14 15:42:55 +0930

    Adding starting code to serve images. Added admin panel.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http, {'pingInterval': 2000, 'pingTimeout': 10000});
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {'pingInterval': 2000, 'pingTimeout': 10000});
 
+const config = require("./config.json");
+const fs = require("fs");
 
 app.use(express.static(__dirname + '/js'));
 
@@ -14,14 +16,19 @@ app.get('/admin', function(req, res) {
     res.sendFile(__dirname + '/admin.html');
 });
 
+
+app.get('/input', function(req, res) {
+    res.sendFile(__dirname + '/input');
+});
+
 let imagePointer = 0;
-let folders = ["one","two","three","four"];
 
 io.on('connection', function(socket) {
+        let files=[];
         // create client id, store it in array, and send it to client
         socket.on('getimage', function () {
             //socket.emit('sent id',socket.id);
-            socket.emit('folders',folders);
+            socket.emit('folders',config.folders);
         });
 
         socket.on('next', function(t) {
@@ -34,6 +41,19 @@ io.on('connection', function(socket) {
           socket.emit('image','');
         });
 
+        socket.on('init', function() {
+            console.log('reading files');
+            let files = fs.readdirSync(config.input);
+            fs.readFile(config.input + '1.JPG', function(err, buf){
+                // it's possible to embed binary data
+                // within arbitrarily-complex objects
+                console.log(buf);
+                io.emit('image', { image: true, buffer: buf });
+                console.log('image file is initialized');
+              });
+            //console.log(`files - ${files}`);
+            //io.emit('image',config.input+files[0]);
+        });
 });
 
 // create server on port 80
