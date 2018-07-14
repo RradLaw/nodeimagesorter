cfa4ecfde51ee53e4242c89a1275abc639c220c2
Jarrad Law <jarrad.law@gmail.com>
2018-07-14 11:55:57 +0930

    Added basic socket connections

--- index.js
+++ index.js
@@ -1,76 +1,41 @@
-// Setup basic express server
 var express = require('express');
 var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+var http = require('http').createServer(app);
+var io = require('socket.io')(http, {'pingInterval': 2000, 'pingTimeout': 10000});
 
-server.listen(port, () => {
-  console.log('Server listening at port %d', port);
+
+app.use(express.static(__dirname + '/js'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-// Routing
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/admin', function(req, res) {
+    res.sendFile(__dirname + '/admin.html');
+});
 
-// Chatroom
+let imagePointer = 0;
+let folders = [];
 
-var numUsers = 0;
+io.on('connection', function(socket) {
+        // create client id, store it in array, and send it to client
+        socket.on('getimage', function () {
+            //socket.emit('sent id',socket.id);
+        });
 
-io.on('connection', (socket) => {
-  var addedUser = false;
+        socket.on('next', function(t) {
+          console.log(`next - ${t}`);
+          socket.emit('image','');
+        });
 
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', (data) => {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
-  });
+        socket.on('prev', function(t) {
+          console.log(`prev - ${t}`);
+          socket.emit('image','');
+        });
 
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
-    if (addedUser) return;
+});
 
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', () => {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', () => {
-    if (addedUser) {
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
-  });
+// create server on port 80
+http.listen(80, function() {
+    console.log('listening on *:80');
 });
